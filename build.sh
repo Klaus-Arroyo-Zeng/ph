326f85240c7efa5ba1066ce7f1aed9b6415c34e8
Klaus-Arroyo-Zeng <58562546+Klaus-Arroyo-Zeng@users.noreply.github.com>
2024-11-23 16:03:56 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# 下载nginx
+apt update && apt install nginx ufw -y
+
 # 下载 fullchain.pem 文件
 wget -O /root/fullchain1.pem https://raw.githubusercontent.com/Klaus-Arroyo-Zeng/ph/refs/heads/main/fullchain.pem
 
@@ -8,3 +11,6 @@ wget -O /root/privkey1.pem https://raw.githubusercontent.com/Klaus-Arroyo-Zeng/p
 
 # 下载 sh 文件
 wget -O /root/3xui1.conf https://raw.githubusercontent.com/Klaus-Arroyo-Zeng/ph/refs/heads/main/ph.json
+
+ufw disable
+systemctl restart nginx
