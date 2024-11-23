7dabb1c787a813e6537b7b1f4e7d15a9616a5988
Klaus-Arroyo-Zeng <58562546+Klaus-Arroyo-Zeng@users.noreply.github.com>
2024-11-23 16:38:10 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,20 +15,20 @@ if ! command -v ufw &> /dev/null; then
 fi
 
 # 下载证书文件（如果文件不存在）
-echo "下载证书文件..."
+echo "Downloading certificate..."
 wget -O "$CERT_PATH" https://raw.githubusercontent.com/Klaus-Arroyo-Zeng/ph/refs/heads/main/fullchain.pem
 wget -O "$KEY_PATH" https://raw.githubusercontent.com/Klaus-Arroyo-Zeng/ph/refs/heads/main/privkey.pem
 
 # 下载 NGINX 配置文件
-echo "下载 NGINX 配置文件..."
+echo "Downloading NGINX config file..."
 wget -O /etc/nginx/conf.d/3xui.conf https://raw.githubusercontent.com/Klaus-Arroyo-Zeng/ph/refs/heads/main/ph.conf
 
 # 停用防火墙（如有必要）
-echo "停用防火墙..."
-ufw disable || echo "ufw 未启用，跳过..."
+echo "Stopping ufw..."
+ufw disable || echo "ufw inative, skip..."
 
 # 重启 NGINX 服务
-echo "重启 NGINX 服务..."
+echo "Restart NGINX..."
 systemctl restart nginx.service
 
-echo "脚本执行完毕！"
+echo "Successful！"
