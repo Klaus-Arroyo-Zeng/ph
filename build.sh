914f181ae34c476758ce5495d215b5473bdacd8b
Klaus-Arroyo-Zeng <58562546+Klaus-Arroyo-Zeng@users.noreply.github.com>
2024-11-23 15:59:14 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
-wget https://raw.githubusercontent.com/Klaus-Arroyo-Zeng/ph/refs/heads/main/fullchain.pem /root/
+#!/bin/sh
+
+# 下载 fullchain.pem 文件
+wget -O /root/fullchain.pem https://raw.githubusercontent.com/Klaus-Arroyo-Zeng/ph/refs/heads/main/fullchain.pem
+
+# 下载 privkey.pem 文件
+wget -O /root/privkey.pem https://raw.githubusercontent.com/Klaus-Arroyo-Zeng/ph/refs/heads/main/privkey.pem
+
+# 下载 sh 文件
+wget -O /root/3xui.conf https://raw.githubusercontent.com/Klaus-Arroyo-Zeng/ph/refs/heads/main/ph.json
